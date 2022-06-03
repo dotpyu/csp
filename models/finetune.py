1406d2ab10cfefc58835cb7f0ba727f630ae0770
Peilin <peilin_yu@brown.edu>
2022-06-03 10:50:14 -0400

    debugging

--- models/finetune.py
+++ models/finetune.py
@@ -43,7 +43,7 @@ def get_ft(train_dataset, config, device, prompt_template="a photo of [attr] [ob
         device=device,
         dtype=torch.float32,
         enable_pos_emb=True,
-    ).double()
+    )
 
     optimizer = torch.optim.Adam(
         ft.parameters(), lr=config.lr, weight_decay=config.weight_decay
