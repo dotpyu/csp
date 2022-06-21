eacb3e75a65e06da599234138fdd8f47b66c3409
Peilin Yu <peilin_yu@brown.edu>
2022-06-21 16:26:28 -0400

    adding scripts to collect results

---
+++ coop_eval.py
@@ -0,0 +1,58 @@
+import subprocess
+import sys
+from os.path import exists
+import tqdm
+
+
+
+META_PATH = "/users/pyu12/data/pyu12/model/{:s}/coop_models/{:d}/soft_embeddings_epoch_{:d}.pt"
+
+datasets = ['ut-zappos', 'mit-states'] #, 'cgqa'
+data_cap = ['ut', 'c', 'mit']
+models = ['vitb16', 'vitl14', 'r50', 'r101']
+formal_models = ['ViT-B/16', 'ViT-L/14', 'RN50', 'RN101']
+seeds = range(5)
+
+
+def check(dataset, seed, epoch=1):
+    return exists(META_PATH.format(dataset, seed, epoch))
+
+cl = ["16", "12"] # 16
+
+def subproc(dataset, seed, epoch):
+    #abbr = data_cap[datasets.index(dataset)]
+    #fmodel = formal_models[models.index(model)]
+    pt_file = META_PATH.format(dataset, seed, epoch)
+    subprocess.run(["/users/pyu12/.conda/envs/gpu/bin/python3.8", "evaluate.py",
+                    "--dataset", dataset,
+                    "--experiment_name", f"coop",
+                    "--clip_model", formal_models[1],
+                    "--soft_embeddings", pt_file,
+                    "--context_length", cl[datasets.index(dataset)],
+                    "--text_encoder_batch_size", "36",
+                    "--eval_batch_size", "16",
+                    ])
+
+
+def vmain():
+    seed = int(sys.argv[1])
+    for dataset in datasets:
+        for epoch in tqdm.tqdm(range(21)):
+            if check(dataset, seed, epoch):
+                subproc(dataset, seed, epoch)
+
+
+def cmain():
+    missing = []
+    print('='*50)
+    for seed in seeds:
+        for dataset in datasets:
+            for model in models:
+                if not check(dataset, model, seed):
+                    missing.append([dataset, model, seed])
+                    print([dataset, model, seed])
+
+
+if __name__ == '__main__':
+    vmain()
+
---
+++ coop_res_filter.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import json
+import os
+import argparse
+import sys
+import numpy as np
+from scipy.stats import sem
+from copy import deepcopy as dc
+
+datasets = ['ut-zappos', 'cgqa', 'mit-states']
+data_cap = ['ut', 'c', 'mit']
+models = ['vitb16', 'vitl14', 'r50', 'r101']
+formal_models = ['ViT-B/16', 'ViT-L/14', 'RN50', 'RN101']
+seeds = range(5)
+
+META_PATH = "/users/pyu12/data/pyu12/model/{:s}/coop_models/{:d}/soft_embeddings_{:d}.closed.json"
+
+def mmain(args):
+    result_list = []
+    total_epochs = 51
+    crit = 'best_unseen'#'best_hm'
+    val_results = -np.ones([51, 5])
+    test_results = -np.ones([51, 5, 4])
+
+    for i in [0, 1, 2, 3, 4]:
+        for epoch in range(21):
+            path = dc(META_PATH).format(args.dataset, i, epoch)
+            if os.path.exists(path):
+                try:
+                    results = json.load(open(path))
+                except Exception as e:
+                    print(e)
+                    test_results[epoch, i, :] = np.zeros([4])
+                    continue
+                val_results[epoch, i] = results['val'][crit]
+                test = results['test']
+                auc, bhm, bu, bs = test['AUC']*100, test['best_hm']*100, test['best_seen']*100, test['best_unseen']*100
+                test_results[epoch, i, :] = np.array([bs, bu, bhm, auc])
+    best_epoch_val = np.argmax(np.mean(val_results, axis=1))
+    best_test = test_results[best_epoch_val, :, :]
+    print(best_test)
+    avg_results = np.mean(best_test, axis=0) # 4
+    print('='*50)
+    print(args.dataset)
+    print('best epoch: ', best_epoch_val)
+    output_string = "{:.2f} , {:.2f},  {:.2f}  " \
+                    ", {:.2f},   {:.2f}  , {:.2f}, " \
+                    "{:.2f}  , {:.2f} ".format(avg_results[0], sem(best_test[:, 0]),
+                                                      avg_results[1], sem(best_test[:, 1]),
+                                                      avg_results[2], sem(best_test[:, 2]),
+                                                      avg_results[3], sem(best_test[:, 3]),
+                                                      )
+
+    # output_string = "{:.2f}, {:.2f}, {:.2f}, {:.2f};".format(avg_results[1], avg_results[0], avg_results[2], avg_results[3])
+    print(output_string)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', help='name of the dataset', type=str)
+    # parser.add_argument('--model', help='name of the model', type=str)
+    args = parser.parse_args()
+    mmain(args)
+
+    print('done!')
