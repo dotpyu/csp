4fe098ddae997db98a2e9c896648112b6c2e87ba
Peilin Yu <peilin_yu@brown.edu>
2022-06-23 15:47:55 -0400

    batched coop eval for cgqa

--- collect_we.py
+++ collect_we.py
@@ -5,8 +5,8 @@ import numpy as np
 import os
 
 default_path = './vocab_results/'
-cc_name = '{:s}_{:s}_seed_{:d}__{:s}.json'
-clip_name = 'clip_{:s}__{:s}.json'
+cc_name = '{:s}{:s}_{:s}_seed_{:d}__{:s}.json'
+clip_name = '{:s}clip_{:s}__{:s}.json'
 
 output_template = "{:.1f},{:.1f},{:.1f},{:.1f},,{:.1f},{:.1f},{:.1f},{:.1f},,{:.1f},{:.1f},{:.1f},{:.1f}"
 clip_template = "{:.1f},,{:.1f},,,{:.1f},,{:.1f},,,{:.1f},,{:.1f}"
@@ -25,25 +25,25 @@ def load_acc(path, checkpoint):
             print(e)
 
 
-def clip_eval(checkpoint):
-    print(f'Performance @ {checkpoint} with CLIP')
+def clip_eval(checkpoint, prefix):
+    print(f'Performance @ {checkpoint} with CLIP {prefix}')
     res = []
     for dataset in datasets:
         for task in tasks:
-            res.append(float(load_acc(default_path+dc(clip_name).format(dataset, task), checkpoint))*100)
+            res.append(float(load_acc(default_path+dc(clip_name).format(prefix, dataset, task), checkpoint))*100)
     print()
     print(dc(clip_template).format(*res))
     print()
     print("= "*50)
 
-def reg_eval(method, checkpoint):
-    print(f'Performance @ {checkpoint} with {method}')
+def reg_eval(method, checkpoint, prefix):
+    print(f'Performance @ {checkpoint} with {method} {prefix}')
     res = []
     for dataset in datasets:
         for task in tasks:
             ir = []
             for seed in seeds:
-                ir.append(float(load_acc(default_path + dc(cc_name).format(method, dataset, seed, task), checkpoint))*100)
+                ir.append(float(load_acc(default_path + dc(cc_name).format(prefix, method, dataset, seed, task), checkpoint))*100)
             res += [np.mean(ir), sem(ir)]
     print()
     print(dc(output_template).format(*res))
@@ -51,9 +51,11 @@ def reg_eval(method, checkpoint):
     print("= " * 50)
 
 if __name__ == '__main__':
+    ckpts = [1]
     for ckpt in ckpts:
-        print(f"Running checkpoint @ {ckpt}")
-        clip_eval(ckpt)
-        reg_eval('coop', ckpt)
-        reg_eval('csp', ckpt)
-        print()
+        for prefix in ['train', '']:
+            print(f"Running checkpoint @ {ckpt}, {prefix}")
+            clip_eval(ckpt, prefix)
+            reg_eval('coop', ckpt, prefix)
+            reg_eval('csp', ckpt, prefix)
+            print()
